2e8a4a3e8cd5b53badb62bab871bfe8091e9e590
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-11-14 11:15:37 -0300

    adiciona opções ao webpack

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,26 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
+  loaders: [
+    {
+      test: /\.vue$/,
+      loader: 'vue',
+    },
+    {
+      test: /\.s[a|c]ss$/,
+      loader: 'style!css!sass',
+    }
+  ],
+  vue: {
+    loaders: {
+      scss: 'style!css!sass',
+    },
+  },
+  sassOptions: {
+    indentedSyntax: true,
+  },
+  plugins: {
     // to edit target browsers: use "browserlist" field in package.json
     "autoprefixer": {}
-  }
+  },
 }
