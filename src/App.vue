4c7e6f2e90be32a4d3b2de80f7f6a5d7f1915124
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-02-23 20:18:14 -0300

    corrige bottom-bar color

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <md-tab md-label="IMC" to="/imc" exact></md-tab>
     </md-tabs>
     <router-view id="main" class="md-layout md-gutter md-alignment-center-center"></router-view>
-    <md-bottom-bar md-sync-route md-type="fixed" md-theme="bottom-bar-light" id="bottom-bar">
+    <md-bottom-bar md-sync-route md-type="fixed" md-theme="bottom-bar-dark" id="bottom-bar">
       <md-bottom-bar-item to="/" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
       <md-bottom-bar-item to="/imc" exact md-label="IMC" md-icon="face"></md-bottom-bar-item>
     </md-bottom-bar>
