323c7338b6b2b6fa4feea8b3c1d8f5a5f6d78962
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-02-23 20:06:35 -0300

    corrige exibição de menu em mobile e desktop

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <!-- <md-tabs md-sync-route md-alignment="centered">
+    <md-tabs md-sync-route md-alignment="centered" id="top-bar">
       <md-tab md-label="Home" to="/" exact></md-tab>
       <md-tab md-label="IMC" to="/imc" exact></md-tab>
-    </md-tabs> -->
+    </md-tabs>
     <router-view id="main" class="md-layout md-gutter md-alignment-center-center"></router-view>
     <md-bottom-bar md-sync-route md-type="fixed" md-theme="bottom-bar-light" id="bottom-bar">
       <md-bottom-bar-item to="/" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
